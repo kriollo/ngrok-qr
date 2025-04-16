212a9a694655a2088406a70c2d2db2e1c027ada3
kriollo <32275426+kriollo@users.noreply.github.com>
2025-04-16 11:57:28 -0400

    Agregar configuración de oxlint y prettier; actualizar package.json para incluir scripts de formato y nuevas dependencias

---
+++ .prettierrc.mjs
@@ -0,0 +1,41 @@
+const config = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+    endOfLine: 'auto',
+    htmlWhitespaceSensitivity: 'ignore',
+    insertPragma: false,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'all',
+    useTabs: false,
+    vueIndentScriptAndStyle: true,
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 4,
+            },
+        },
+        {
+            parser: 'php',
+            files: '*.php',
+            options: {
+                braceStyle: 'psr-2',
+                printWidth: 120,
+                tabWidth: 4,
+                trailingCommaPHP: true,
+            },
+        },
+    ],
+};
+
+export default config;
